66daf3a85e294eed0fb52fa704f5a3ab2843414a
Abdulqadir90 <33445223+Abdulqadir90@users.noreply.github.com>
2021-06-11 16:38:47 +0100

    adding handlersubmit

--- CreateMeow.js
+++ CreateMeow.js
@@ -2,10 +2,28 @@ import React, {useState} from 'react'
 
 const CreateMeow = () => {
     const [textArea, setTextArea] = useState("");
+    const [hashtag, setHashtag] = useState("");
+
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        fetch(
+            "https://cyf-cors-api.herokuapp.com/meow",
+            {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json"
+            },
+            body: JSON.stringify({textArea, hashtag})
+         })
+         ````
+    }
 
     return (
         <form>
-            <textarea onChange={(e) => setTextArea(e.target.value)}></textarea>
+            <textarea  value={textArea} onChange={(e) => setTextArea(e.target.value)}></textarea>
+            <input type="text" value={hashtag} onChange={(e) => setHashtag(e.target.value)}></input>
+            <button type="submitting"  onSubmit={handleSubmit}></button>
             
         </form>
     )
